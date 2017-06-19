a232f2b4936f1fba572ec5525c8b6c2a84c6f90b
ryanml <rlanese@asu.edu>
2017-06-19 09:46:51 -0700

    Building parsed css in to a simple struct

--- prettifier.py
+++ prettifier.py
@@ -1,4 +1,5 @@
 # Author: Ryan Lanese
+import tinycss as tc
 from sys import argv
 
 USAGE_TEXT = """
@@ -9,11 +10,22 @@ pprettifier [-f|-t] [style.css|RAW INPUT]
 """
 
 """
+Invalid stylesheet error
+"""
+class InvalidStylesheetError(Exception):
+
+    def __init__(self):
+        super(
+            InvalidStylesheetError, self
+        ).__init__("Invalid CSS!")
+
+"""
 Args error
 """
 class InvalidArgumentsError(Exception):
 
     def __init__(self):
+        print USAGE_TEXT
         super(
             InvalidArgumentsError, self
         ).__init__("Invalid arguments!")
@@ -24,18 +36,38 @@ Prettifier
 class Prettifier(object):
 
     def __init__(self, file, extra=False):
-        self.rules = self.load_file(file)
+        self.parser = tc.make_parser()
+        self.styles = self.build_struct(
+            self.load_file(file)
+        )
+
+    def build_struct(self, rules):
+        styles = []
+        for rule in rules.rules:
+            declarations = []
+            selector = rule.selector.as_css()
+            _declarations = rule.declarations
+            for declaration in _declarations:
+                name = declaration.name
+                value = declaration.value.as_css()
+                declarations.append(
+                    {'declaration': ("%s:%s" % (name, value))}
+                )
+            styles.append(
+                {'selector': selector, 'declarations': declarations }
+            )
+        return styles
 
     def load_file(self, file):
-        rules = []
-        with open(file, 'r') as f:
-            for line in f:
-                rules.append(f)
+        with open(file) as f:
+            lines = f.read()
+            rules = self.parser.parse_stylesheet(lines)
+        if len(rules.errors) > 0:
+            raise InvalidStylesheetError
         return rules
 
 def main():
-    style_sheet = '/path/to/style.css'
-    #prettifier = Prettifier(style_sheet)
+    prettifier = Prettifier(argv[2])
 
 if __name__ == "__main__":
     error_conditions = [
@@ -43,6 +75,5 @@ if __name__ == "__main__":
         (argv[1] != '-f' and argv[1] != '-t')
     ]
     if True in error_conditions:
-        print USAGE_TEXT
         raise InvalidArgumentsError()
     main()
